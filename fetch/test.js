665903351da1a8ef57c14ab506408ee77ee8f1f4
linteng <875482941@qq.com>
2017-07-13 17:32:18 +0800

    update

--- fetch/test.js
+++
@@ -1,8 +0,0 @@
-/**
- *
- * @authors Lincoln (875482941@qq.com)
- * @date    2017-06-22 21:24:33
- * @version $Id$
- */
-
-fdasaffafafa
--- test.js
+++
@@ -1,7 +0,0 @@
-/**
- *
- * @authors Lincoln (875482941@qq.com)
- * @date    2017-06-22 21:26:52
- * @version $Id$
- */
-
