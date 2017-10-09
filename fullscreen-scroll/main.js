d4fc10a96f1f242f7bc0d1bd31d3b6ab6c1e95ec
linteng <875482941@qq.com>
2017-10-09 20:40:33 +0800

    update 全屏滚动组件

--- fullscreen-scroll/main.js
+++ fullscreen-scroll/main.js
@@ -5,6 +5,7 @@
  */
 'use strict'
 
+var pageNums = 0
 var listen = function(el, ev, fn, capture){
     if(window.addEventListener){
         el.addEventListener(ev, fn, capture)
@@ -23,7 +24,7 @@ var remove = function(el, ev, fn, capture){
     }
 }
 
-function watch(obj, prop, cb){
+var watch = function(obj, prop, cb){
     var oldVal = ''
     if(prop in obj){
         oldVal = obj[prop]
@@ -39,100 +40,131 @@ function watch(obj, prop, cb){
     })
 }
 
-var scroll = function(parendId, childId, time){
-
-}
-
-scroll.prototype.curPage = 0
-scroll.prototype.$el = function(parendId, childId, time){
-
-    var parentDom = document.getElementById(parendId)
-    parentDom.style.overflow = 'hidden'
-
-    var childDom = parentDom.querySelectorAll('.' + childId)
-
-    var container = document.createElement('section')
-    var btnGroup = document.createElement('section')
-
-    var span = document.createElement('span')
-    var frag = document.createDocumentFragment()
-    var transition = time.indexOf('s') < 0 ? time + 's' : time
-    var _this = this
-    for(var i = 0;i < childDom.length;i++){
-        container.appendChild(childDom[i])
-        var btn = span.cloneNode(false)
-        btn.setAttribute('style', 'display: inline-block;width: 10px;height: 10px;border-radius: 100%;background: rgba(255,255,255,.6);transition: .4s;')
-        if(i == 0)
-            btn.style.background = '#f30'
-        ;(function(i){
-            listen(btn, 'click', function(){
-                // console.log(i);
-                _this.curPage = i
-                _this.container.style.transform = 'translate(0px, ' + -_this.height * _this.curPage + 'px)'
-            })
-        })(i);
-        frag.appendChild(btn)
-    }
-    btnGroup.appendChild(frag)
-
-    container.style.width = '100%'
-    container.style.height = '100%'
-    container.style.transition = time.indexOf('s') < 0 ? time + 's' : time
-
-    btnGroup.setAttribute('style', 'position: absolute;right: 10px;top: 50%;width: 10px;text-align: center;transform: translate(0, -50%);transition: '+ transition +' ')
-
-    parentDom.style.position = 'relative'
-    parentDom.innerHTML = ''
-    parentDom.appendChild(container)
-    parentDom.appendChild(btnGroup)
-    return {parentDom: parentDom, childDom: childDom, container: container, btnGroup: btnGroup}
-}
-scroll.prototype.init = function(parendId, childId, time){
-
-    var el = this.$el(parendId, childId, time)
-    this.parentDom = el.parentDom
-    this.childDom = el.childDom
-    this.container = el.container
-    this.btnGroup = el.btnGroup
-
-    this.height = (window.getComputedStyle ? (window.getComputedStyle(this.parentDom).height).replace('px', '') : this.parentDom.offsetHeight) >> 0
-
-    var mouseWheelEv = /Firefox/i.test(navigator.userAgent) ? "DOMMouseScroll": "mousewheel",
-    direction = /Firefox/i.test(navigator.userAgent) ? "detail": "deltaY",
-    now = Date.now()
-    var _this = this
-    listen(this.parentDom, mouseWheelEv, function(ev){
-        if(Date.now() - now > 500){
-            if(ev[direction] > 0){
-                if(_this.curPage + 1 < _this.childDom.length){
-                    _this.curPage++
-                }else{
-                    _this.curPage = 0
-                }
-            }else{
-                if(_this.curPage - 1 >= 0){
-                    _this.curPage--
-                }else{
-                    _this.curPage = _this.childDom.length - 1
-                }
-
-            }
-            _this.container.style.transform = 'translate(0px, ' + -_this.height * _this.curPage + 'px)'
-            now = Date.now()
+/**
+ * [监听对象所有属性变化]
+ * @param  {Object}   obj [要监听属性的对象]
+ * @param  {Function} cb  [description]
+ */
+var observable = function(obj, prop, cb){
+    var type = Object.prototype.toString.call(prop)
+    if(type === '[object String]'){
+        watch(obj, prop, cb)
+    }else if(type === '[object Function]'){
+        var keys = Object.keys(obj)
+        cb = prop
+        for(var i = 0;i < keys.length;i++){
+            watch(obj, keys[i], cb)
         }
-    })
+    }
+
 }
 
+var scroll = function(){}
+
+scroll.prototype = {
+    curPage: 0,
+    $el: function(parendId, childId, time){
+
+        var parentDom = document.getElementById(parendId)
+        parentDom.style.overflow = 'hidden'
+
+        var childDom = parentDom.querySelectorAll('.' + childId)
+        var container = document.createElement('section')
+        var btnGroup = document.createElement('section')
+
+        var span = document.createElement('span')
+        var frag = document.createDocumentFragment()
+        var transition = time.indexOf('s') < 0 ? time + 's' : time
+        var _this = this
+        for(var i = 0;i < childDom.length;i++){
+            container.appendChild(childDom[i])
+            var btn = span.cloneNode(false)
+            btn.setAttribute('style', 'display: inline-block;width: 10px;height: 10px;border-radius: 100%;background: rgba(255,255,255,.6);transition: .4s;')
+            if(i == 0){
+                btn.style.background = '#f30'
+                btn.style.transform = 'scale(1.5)'
+            }
+            ;(function(i){
+                listen(btn, 'click', function(){
+                    _this.curPage = i
+                    container.style.transform = 'translate(0px, ' + -_this.curHeight * _this.curPage + 'px)'
+                })
+            })(i);
+            frag.appendChild(btn)
+        }
+        btnGroup.appendChild(frag)
+        btnGroup.setAttribute('style', 'position: absolute;right: 10px;top: 50%;width: 10px;text-align: center;transform: translate(0, -50%);transition: '+ transition +' ')
+        btnGroup.id = 'ui-fullpage-btn-' + pageNums
+
+        container.style.width = '100%'
+        container.style.height = '100%'
+        container.style.transition = time.indexOf('s') < 0 ? time + 's' : time
+        container.id = 'pageName-' + pageNums
+
+
+        parentDom.style.position = 'relative'
+        parentDom.appendChild(container)
+        parentDom.appendChild(btnGroup)
+        return parentDom
+    },
+    init: function(parendId, childId, time){
+        pageNums++
+        this.el = this.$el(parendId, childId, time)
+        var container = document.querySelector('.' + childId).parentNode,
+        childDom = container.childNodes
+
+        this.curHeight = (window.getComputedStyle ? (window.getComputedStyle(this.el).height).replace('px', '') : this.el.offsetHeight) >> 0
+
+        var mouseWheelEv = /Firefox/i.test(navigator.userAgent) ? "DOMMouseScroll": "mousewheel",
+        direction = /Firefox/i.test(navigator.userAgent) ? "detail": "deltaY",
+        now = Date.now()
+        var _this = this
+        listen(this.el, mouseWheelEv, function(ev){
+            if(Date.now() - now > 1000){
+                if(ev[direction] > 0){
+                    if(_this.curPage + 1 < childDom.length){
+                        _this.curPage++
+                    }else{
+                        _this.curPage = 0
+                    }
+                }else{
+                    if(_this.curPage - 1 >= 0){
+                        _this.curPage--
+                    }else{
+                        _this.curPage = childDom.length - 1
+                    }
+
+                }
+                container.style.transform = 'translate(0px, ' + -_this.curHeight * _this.curPage + 'px)'
+                now = Date.now()
+            }
+        })
+
+        listen(window, 'resize', function(){
+
+            _this.curHeight = (window.getComputedStyle ? (window.getComputedStyle(_this.el).height).replace('px', '') : _this.el.offsetHeight) >> 0
+            container.style.transition = '0s'
+            container.style.transform = 'translate(0px, ' + -_this.curHeight * _this.curPage + 'px)'
+            setTimeout(function(){
+                container.style.transition = time.indexOf('s') < 0 ? time + 's' : time
+            }, 0)
+        })
+    }
+}
 
 
 window.fullPage = new scroll()
-watch(fullPage, 'curPage', function(index){
-    var span = fullPage.btnGroup.childNodes
+observable(fullPage['__proto__'], 'curPage', function(index){
+    var btnGroup = document.getElementById('ui-fullpage-btn-' + pageNums),
+        span = btnGroup.childNodes
+
     for(var i = 0;i < span.length;i++){
         if(i == index){
             span[index].style.background = '#f30'
+            span[index].style.transform = 'scale(1.5)'
         }else{
             span[i].style.background = 'rgba(255,255,255,.6)'
+            span[i].style.transform = ''
         }
     }
 
